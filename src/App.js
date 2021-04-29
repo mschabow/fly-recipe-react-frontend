8633a45b8ddd268d6784f43261dd3e40a0359aa2
Matt Schabowsky <mschabowsky@gmail.com>
2021-04-29 16:37:21 -0600

    Working Authentication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { useState, useEffect } from "react";
-import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
-import { GetCompleteRecipes } from "./Hooks/GetCompleteRecipes";
+import {AmplifySignOut, AmplifyAuthenticator, AmplifySignIn, AmplifySignUp } from "@aws-amplify/ui-react";
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
 import MainPage from "./Pages/MainPage";
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
 
 function App() {
   const [recipes, setRecipes] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [authState, setAuthState] = React.useState();
+  const [user, setUser] = React.useState();
+
+  React.useEffect(() => {
+      return onAuthUIStateChange((nextAuthState, authData) => {
+          setAuthState(nextAuthState);
+          setUser(authData)
+      });
+  }, []);
 
   //GetCompleteRecipes(setRecipes, setLoading);
   useEffect(() => {
@@ -20,8 +32,44 @@ function App() {
     fetchRecipes();
   }, []);
 
-  return <>{loading ? "Loading..." : <MainPage recipes={recipes} />}</>;
+  return authState === AuthState.SignedIn && user ? (
+    <div className="App">
+        {console.log(user)}
+        <div>Hello, {user.attributes.name}</div>
+        <>{loading ? "Loading..." : <MainPage recipes={recipes} />}</>
+        <AmplifySignOut />
+    </div>
+  ) : (
+    <AmplifyAuthenticator usernameAlias="email">
+      <AmplifySignUp
+        slot="sign-up"
+        usernameAlias="email"
+        formFields={[
+          {
+            type: "email",
+            label: "Email Address: (used as username)",
+            placeholder: "name@something.com",
+            required: true,
+          },
+          {
+            type: "password",
+            label: "Password",
+            placeholder: "password",
+            required: true,
+          },
+          {
+            type: "name",
+            label: "First and Last Name",
+            placeholder: "John Doe",
+            required: true,
+          },
+        ]} 
+      />
+      <AmplifySignIn slot="sign-in" usernameAlias="email" />
+    </AmplifyAuthenticator>
+);
 }
 
-//export default App;
-export default withAuthenticator(App);
+
+export default App;
+
